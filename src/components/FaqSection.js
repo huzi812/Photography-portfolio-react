06d203a453e1c5789a8298332b65550fb5c3acd9
Huzaifa habib <huzimoon812@gmail.com>
2021-08-16 15:43:55 +0500

    adding faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const faqSection = () => {
+
+    return (
+        <div className="faq">
+            <h2>Any question <span>FAQ</span></h2>
+            <div className="question">
+                <h4>how do i start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit, ametelit.</p>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>how do i start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit, ametelit.</p>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>how do i start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit, ametelit.</p>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>how do i start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit, ametelit.</p>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+
+        </div>
+    );
+
+
+};
+export default faqSection;
--- src/pages/AboutUS.js
+++ src/pages/AboutUS.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AboutSection  from "../components/AboutSection";
 import ServicesSection  from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs= ()=>{
 
@@ -8,6 +9,7 @@ return(
 <div className="AboutUs">
 <AboutSection  />
 <ServicesSection />
+<FaqSection/>
 </div>
 );
 
