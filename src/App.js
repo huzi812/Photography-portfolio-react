0403bcafecb8c063403472ff536bb16915a704d2
Huzaifa habib <huzimoon812@gmail.com>
2021-08-16 14:48:36 +0500

    changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutUs from "./pages/AboutUS";
+
+function App() {
+  return (
+    <div className="App">
+      <AboutUs/>
+    </div>
+  );
+}
+
+export default App;
